6869a0a289dfd05a0105782da4789aeb86dd5730
unknown <umerfsniper@gmail.com>
2024-08-10 05:43:59 -0700

    message

---
+++ javascript/Operator/script.js
@@ -0,0 +1,64 @@
+// Binary Operator
+// Comparsion Operator
+// > < == >= <= === != !==
+let a=5
+console.log(a==5);
+
+
+
+
+
+
+
+
+
+
+
+// Airthmetic Assignment OPerator
+// let a=5
+// let b=4
+// b+=a // b= b+a
+// console.log(a);
+
+
+
+
+
+
+
+
+
+// Airthemtic Operator
+// + - * / %
+// let a=2
+// let b=5
+// a=a+b
+// //7+5
+// b=a+b
+// console.log(b);
+
+// let c=a+b
+// let d=a-b
+// let e=a*b
+// let f=a/b
+// let g=a%b
+
+
+
+
+
+
+
+
+
+
+// Unary Operator
+// Pre / Post
+// let a=5
+// ++a
+// a++
+// let b=a
+// b--
+// a=b
+// console.log(a);
+
---
+++ javascript/alert/script.js
@@ -0,0 +1,18 @@
+// alert("Be Silence")
+// alert("Smoking is Dangerious for health")
+// confirm("message")
+// document.write("Write in a page")
+// document.write("Not Recommended")
+// console.log("Print Here")
+
+// Variable For String
+// let names="Sawai"
+// let nick=names
+// console.log(nick);
+
+// Variable For Numbers
+let a=6
+let b=41302-1538485
+
+console.log(b);
+
